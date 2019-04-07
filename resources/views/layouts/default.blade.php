43c64afe27ee393e1eb3f2e8cefddc4c3baca5db
vagrant <vagrant@homestead>
2019-04-07 10:48:24 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Weibo App') - Laravel 教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <!-- 导航栏 -->
