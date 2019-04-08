10a3b94d0d6e2d5045a9c9366eec0e18bcc738f1
vagrant <vagrant@homestead>
2019-04-08 12:29:56 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,26 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    // 注册表单
     public function create()
     {
         return view('users.create');
     }
 
+    // 展示
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // 注册提交
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
