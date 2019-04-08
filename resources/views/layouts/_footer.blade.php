fff8346f41c408a4139e6eab0ac4bd4d9d14fb07
vagrant <vagrant@homestead>
2019-04-08 07:10:26 +0000

    用户注册表单

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="brand-icon">
+    <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" height="13px" alt="brand-icon">
     <a href="https://learnku.com/laravel/courses" target="_blank">
         刻意练习, 每日精进
     </a>
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,10 +1,34 @@
 @extends('layouts.default')
 @section('title', '注册')
 @section('content')
-    <h1>注册账号</h1>
-    <form action="{{ route('signup') }}" method="post">
-        <p>用户名: <input type="text" placeholder="请输入用户名"></p>
-        <p>密  码: <input type="text" placeholder="请输入密码"></p>
-        <p>邮 箱: <input type="text" placeholder="请输入验证码"></p>
-    </form>
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header"><h5>注册</h5></div>
+            <div class="card-body">
+                <form action="{{ route('users.store') }}" method="post">
+                    <div class="form-group">
+                        <label for="name">名称:</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">邮箱:</label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">密码:</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码:</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
