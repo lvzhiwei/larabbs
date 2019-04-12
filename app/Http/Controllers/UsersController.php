d956073aa54e1709bcd94b170ff25391a51bdb23
vagrant <vagrant@homestead>
2019-04-12 00:00:14 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,6 +43,27 @@ class UsersController extends Controller
     // 用户编辑表单
     public function edit(User $user)
     {
-        return view('user.edit', compact('user'));
+        return view('users.edit', compact('user'));
     }
+
+    // 更新用户信息
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功! ');
+
+        return redirect()->route('users.show', $user);
+    }
+
+
 }
